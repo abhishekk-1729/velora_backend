de282da97f6850b9e9742f19b4ee43547455897f
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2025-01-03 22:59:17 +0530

    added / for health checks

--- chatbot/chatbot/urls.py
+++ chatbot/chatbot/urls.py
@@ -2,8 +2,14 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+# Define the view for the root path
+def root_view(request):
+    return HttpResponse("<h1>Congrats, Backend is running well !!</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('chat.urls')),  # Include the URLs from the chat app
+    path('', root_view),  # Add the root view for the '/' path
+    path('api/v1/chat', include('chat.urls')),  # Include the URLs from the chat app
 ]
