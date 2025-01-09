c8fefd028ba9cf0f1f0b24651d7be8ae4768ada8
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2025-01-09 11:26:20 +0530

    messgae on join chat

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,10 +1,33 @@
 const Chat = require('../models/chat');
 const { io } = require('../index'); // Or wherever your server setup is
 const nodemailer = require('nodemailer');
+const twilio = require('twilio');
+
+// Load Twilio credentials from environment variables
+const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from Twilio
+const authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from Twilio
+const smsNumber = process.env.TWILIO_SMS_NUMBER; // Your Twilio WhatsApp number
+
 // Start a new chat session
 exports.startChat = async (req, res) => {
   const chat = new Chat();
   await chat.save();
+  try {
+    // Initialize the Twilio client
+    const client = twilio(accountSid, authToken);
+
+    // Sending SMS using Twilio API
+    const result = await client.messages.create({
+        from: smsNumber,      // Your Twilio phone number (e.g., +1234567890)
+        to: "+918755273773",            // Recipient's phone number
+        body: "Joined the chat. Go to www.thefirstweb.com/adminChat",        // The message body (text) you want to send
+    });
+} catch (error) {
+    // Handle errors and respond with failure message
+    console.log(error)
+}
+
+
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com", // Gmail SMTP server
     port: 587, // Port for TLS/STARTTLS
